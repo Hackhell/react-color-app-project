6a6eb4f6371a71dc062133da1fbfe55d5c3ad60d
Hackhell <hhelly28@gmail.com>
2020-05-18 12:39:30 +0300

    Fix react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
 
-function App() {
-  return (
-    <Switch>
-    <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>}/>
-    <Route exact path='/palette/:id' render={() => <h1>INDIVIDUAL PALETTE</h1>}/>
-    </Switch>
+class App extends Component {
+  findPalette (id) {
+   return seedColors.find(function(palette) {
+      return palette.id === id
+    })
+  }
 
-/*    <div>
-      <Palette palette={generatePalette(seedColors[4])}
+  render() {
+    return (
+      <Switch>
+      <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>}/>
+      <Route 
+        exact 
+        path='/palette/:id' 
+        render={routeProps => (
+          <Palette palette={generatePalette(
+            this.findPalette(routeProps.match.params.id)
+            )} /> 
+      )}
       />
-    </div> */
-  );
+      </Switch>
+  
+  /*    <div>
+        <Palette palette={generatePalette(seedColors[4])}
+        />
+      </div> */  
+    )
+  }
 }
-
+  
 export default App;
--- src/colorHelper.js
+++ src/colorHelper.js
@@ -29,33 +29,37 @@ import chroma from 'chroma-js';
 
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
-function generatePalette(staterPalette) {
+function generatePalette(starterPalette) {
     let newPalette = {
-        paletteName: staterPalette.paletteName,
-        id: staterPalette.id,
-        emoji: staterPalette.emoji,
+        paletteName: starterPalette.paletteName,
+        id: starterPalette.id,
+        emoji: starterPalette.emoji,
         colors: {}
-    }
-for (let level of levels) {
-    newPalette.colors[level] = [];
-}
+    };
 
-for (let color of staterPalette.colors) {
-    let scale = getScale(color.color, 10).reverse();
-    for (let i in scale) {
-        newPalette.colors[levels[i]].push({
-            name: `${color.name} ${levels[i]}`,
-            id: color.name.toLowerCase().replace(/ /g, '-'),
-            hex: scale[i],
-            rgb: chroma(scale[i]).css(),
-            rgba: chroma(scale[i]).css().replace('rgb', 'rgba').replace(')', ',1.0)')
-        })
+    for (let level of levels) {
+        newPalette.colors[level] = [];
+    }
+
+    for (let color of starterPalette.colors) {
+        let scale = getScale(color.color, 10).reverse();
+    
+            for (let i in scale) {
+                newPalette.colors[levels[i]].push({
+                name: `${color.name} ${levels[i]}`,
+                id: color.name.toLowerCase().replace(/ /g, '-'),
+                hex: scale[i],
+                rgb: chroma(scale[i]).css(),
+                rgba: chroma(scale[i])  
+                    .css()
+                    .replace('rgb', 'rgba')
+                    .replace(')', ',1.0)')
+        });
     }
 }
 
 return newPalette;
 }
-
 function getRange(hexColor) {
     const end = '#fff';
     return [
@@ -64,7 +68,7 @@ function getRange(hexColor) {
             .hex(),
         hexColor,
         end
-    ]
+    ];
 }
 
 function getScale(hexColor, numberOfColors) {
